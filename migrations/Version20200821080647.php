707f2a06019570118cf3ef643f2d9449245b5377
Pop Sorin <popsorin15@gmail.com>
2020-08-21 11:50:52 +0300

    Fix hectare and Mc rounding up the values

---
+++ migrations/Version20200821080647.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200821080647 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE document CHANGE hectare hectare FLOAT NOT NULL, CHANGE cube_meters cube_meters FLOAT NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE document CHANGE hectare hectare INT NOT NULL, CHANGE cube_meters cube_meters INT NOT NULL');
+    }
+}
